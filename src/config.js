088ed73e758fef2fa7cc76041bb330dc21ec708f
Victor Lins <vrlins001@gmail.com>
2026-04-07 21:54:57 -0300

    test: adding secret for testing (trivy)

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    AWS_ACCESS_KEY_ID: "AKIAIOSFODNN7EXAMPLE",
+    AWS_SECRET_ACCESS_KEY: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+    DB_PASSWORD: "super_secret_123"
+};
+
+module.exports = config;
